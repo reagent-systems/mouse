2ba02e4bac9b6414cbb862f09b540c1597b2d61e
ThyFriendlyFox <thyfriendlyfox@gmail.com>
2026-07-10 22:33:22 -0400

    Cleaned Up Workspace

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import { App } from './app.ts'
-import { maybeInstallMockGitHub } from './platform/mockGitHub.ts'
-
-// Install canned GitHub transport before the app boots, when ?mockgh=1.
-maybeInstallMockGitHub()
-
-new App(document.querySelector<HTMLDivElement>('#app')!)
